9f339e3f52f3a1c54df3f232eee3957f41fe36a6
gromkon <steamgrom@mail.ru>
2020-05-11 16:33:28 +0300

    Homework 2

---
+++ core/src/ru/gromkon/sprite/Background.java
@@ -0,0 +1,20 @@
+package ru.gromkon.sprite;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import ru.gromkon.base.Sprite;
+import ru.gromkon.math.Rect;
+
+public class Background extends Sprite {
+
+    public Background(Texture texture) {
+        super(new TextureRegion(texture));
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        setHeightProportion(1f);
+        this.pos.set(worldBounds.pos);
+    }
+}
